15e0c31d59fe6668e9e295e95366a68f928dd4c3
Coen Hallie <me@coenhallie.com>
2022-11-23 15:48:42 +0100

    small changes

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 import inquirer from "inquirer";
 import chalkAnimation from "chalk-animation";
+import figlet from "figlet";
+import gradient from "gradient-string";
+import { createSpinner } from 'nanospinner'
 import { writeFile, readdir, readFile } from "fs/promises";
 import * as path from 'path'
+import { exec } from 'child_process';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 
@@ -10,8 +14,6 @@ const __dirname = path.dirname(__filename);
 
 const configFiles = {};
 const configFolderPath = path.resolve(__dirname, 'config');
-
-const defaultConfig = { "technology": "Default" };
 
 const sleep = (ms = 2000) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -26,31 +28,88 @@ await welcomeAnimation();
 (async () => {
 
   const files = await readdir(configFolderPath).catch(console.log);
-  const useDefault = process.argv[2] === "-y";
 
   for (let i of files) {
-    // framework name is situated between 2 dots eg- react between 2 '.'(s)
     const frameworkName = i.split('.')[1];
     configFiles[frameworkName] = path.join(configFolderPath, i);
   }
 
-  const { technology } = useDefault ? defaultConfig : await inquirer.prompt([
+  const { projectName } = await inquirer.prompt([
+    {
+      type: "input",
+      message: "What's the name of your project?",
+      name: "projectName"
+    }
+  ])
+
+  const { cssFramework } = await inquirer.prompt([
     {
       type: "list",
       message: "Which Tailwind config do you want to use?",
-      name: "technology",
+      name: "cssFramework",
       choices: Object.keys(configFiles),
     }
   ])
 
-  let config = await readFile(configFiles[technology]).catch(console.log);
+  const { sassPrompt } = await inquirer.prompt([
+    {
+      type: "confirm",
+      message: "Would you like to install SASS?",
+      name: "sassPrompt",
+    }
+  ])
 
-  const tailwindConfig = path.join(process.cwd(), 'tailwind.config.js');
+  const { javascriptFramework } = await inquirer.prompt([
+    {
+      type: "list",
+      message: "Which framework do you like to use?",
+      name: "javascriptFramework",
+      choices: ["Nuxt", "Vue 3"],
+    }
+  ])
+
+  // installing framework dependencies
+  const installDependencies = async (javascriptFramework) => {
+    const installCommand = javascriptFramework === "Nuxt" ? `npx nuxi init ${projectName} npm install -D tailwindcss` : "npm install vue@next --save-dev";
+    exec(installCommand, (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    });
+  };
+
+  // install dependencies
+  await installDependencies(javascriptFramework);
+
+  let config = await readFile(configFiles[cssFramework]).catch(console.log);
+
+  const tailwindConfig = path.join(__dirname, 'tailwind.config.js');
 
   await writeFile(tailwindConfig, config.toString()).catch(err => {
     console.log(err);
     process.exit();
   });
 
-  console.log("tailwind.config.json successfully created");
+  const installSpinner = async () => {
+    const spinner = createSpinner('installing....').start();
+    await sleep()
+    spinner.stop()
+  }
+
+  await installSpinner();
+
+  const installComplete = async () => {
+    const message = 'Installation complete!';
+    figlet(message, (err, data) => {
+      console.log(gradient.pastel.multiline(data));
+    });
+  };
+
+  await installComplete();
 })();
